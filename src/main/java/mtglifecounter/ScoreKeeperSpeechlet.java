454191386f8a9c03003c5687ba6956465275c691
Brandon Sheffield <bsheffield@ispatechnology.com>
2016-02-01 18:59:44 -0600

    initial commit.

---
+++ src/main/java/mtglifecounter/ScoreKeeperSpeechlet.java
@@ -0,0 +1,104 @@
+package mtglifecounter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.amazon.speech.slu.Intent;
+import com.amazon.speech.speechlet.IntentRequest;
+import com.amazon.speech.speechlet.LaunchRequest;
+import com.amazon.speech.speechlet.Session;
+import com.amazon.speech.speechlet.SessionEndedRequest;
+import com.amazon.speech.speechlet.SessionStartedRequest;
+import com.amazon.speech.speechlet.Speechlet;
+import com.amazon.speech.speechlet.SpeechletException;
+import com.amazon.speech.speechlet.SpeechletResponse;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+
+public class ScoreKeeperSpeechlet implements Speechlet {
+    private static final Logger log = LoggerFactory.getLogger(ScoreKeeperSpeechlet.class);
+
+    private AmazonDynamoDBClient amazonDynamoDBClient;
+
+    private ScoreKeeperManager scoreKeeperManager;
+
+    private SkillContext skillContext;
+
+    @Override
+    public void onSessionStarted(final SessionStartedRequest request, final Session session)
+            throws SpeechletException {
+        log.info("onSessionStarted requestId={}, sessionId={}", request.getRequestId(),
+                session.getSessionId());
+
+        initializeComponents();
+
+        // if user said a one shot command that triggered an intent event,
+        // it will start a new session, and then we should avoid speaking too many words.
+        skillContext.setNeedsMoreHelp(false);
+    }
+
+    @Override
+    public SpeechletResponse onLaunch(final LaunchRequest request, final Session session)
+            throws SpeechletException {
+        log.info("onLaunch requestId={}, sessionId={}", request.getRequestId(),
+                session.getSessionId());
+
+        skillContext.setNeedsMoreHelp(true);
+        return scoreKeeperManager.getLaunchResponse(request, session);
+    }
+
+    @Override
+    public SpeechletResponse onIntent(IntentRequest request, Session session)
+            throws SpeechletException {
+        log.info("onIntent requestId={}, sessionId={}", request.getRequestId(),
+                session.getSessionId());
+        initializeComponents();
+
+        Intent intent = request.getIntent();
+        if ("NewGameIntent".equals(intent.getName())) {
+            return scoreKeeperManager.getNewGameIntentResponse(session, skillContext);
+
+        } else if ("AddPlayerIntent".equals(intent.getName())) {
+            return scoreKeeperManager.getAddPlayerIntentResponse(intent, session, skillContext);
+
+        } else if ("AddScoreIntent".equals(intent.getName())) {
+            return scoreKeeperManager.getAddScoreIntentResponse(intent, session, skillContext);
+
+        } else if ("TellScoresIntent".equals(intent.getName())) {
+            return scoreKeeperManager.getTellScoresIntentResponse(intent, session);
+
+        } else if ("ResetPlayersIntent".equals(intent.getName())) {
+            return scoreKeeperManager.getResetPlayersIntentResponse(intent, session);
+
+        } else if ("AMAZON.HelpIntent".equals(intent.getName())) {
+            return scoreKeeperManager.getHelpIntentResponse(intent, session, skillContext);
+
+        } else if ("AMAZON.CancelIntent".equals(intent.getName())) {
+            return scoreKeeperManager.getExitIntentResponse(intent, session, skillContext);
+
+        } else if ("AMAZON.StopIntent".equals(intent.getName())) {
+            return scoreKeeperManager.getExitIntentResponse(intent, session, skillContext);
+
+        } else {
+            throw new IllegalArgumentException("Unrecognized intent: " + intent.getName());
+        }
+    }
+
+    @Override
+    public void onSessionEnded(final SessionEndedRequest request, final Session session)
+            throws SpeechletException {
+        log.info("onSessionEnded requestId={}, sessionId={}", request.getRequestId(),
+                session.getSessionId());
+        // any cleanup logic goes here
+    }
+
+    /**
+     * Initializes the instance components if needed.
+     */
+    private void initializeComponents() {
+        if (amazonDynamoDBClient == null) {
+            amazonDynamoDBClient = new AmazonDynamoDBClient();
+            scoreKeeperManager = new ScoreKeeperManager(amazonDynamoDBClient);
+            skillContext = new SkillContext();
+        }
+    }
+}
---
+++ src/main/java/mtglifecounter/ScoreKeeperTextUtil.java
@@ -0,0 +1,58 @@
+package mtglifecounter;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Util containing various text related utils.
+ */
+public final class ScoreKeeperTextUtil {
+
+    private ScoreKeeperTextUtil() {
+    }
+
+    /**
+     * List of player names blacklisted for this app.
+     */
+    private static final List<String> NAME_BLACKLIST = Arrays.asList("player", "players");
+
+    /**
+     * Text of complete help.
+     */
+    public static final String COMPLETE_HELP =
+            "Here's some things you can say. Add John, give John 5 points, tell me the score, "
+                    + "new game, reset, and exit.";
+
+    /**
+     * Text of next help.
+     */
+    public static final String NEXT_HELP = "You can give a player points, add a player, get the "
+            + "current score, or say help. What would you like?";
+
+    /**
+     * Cleans up the player name, and sanitizes it against the blacklist.
+     *
+     * @param recognizedPlayerName
+     * @return
+     */
+    public static String getPlayerName(String recognizedPlayerName) {
+        if (recognizedPlayerName == null || recognizedPlayerName.isEmpty()) {
+            return null;
+        }
+
+        String cleanedName;
+        if (recognizedPlayerName.contains(" ")) {
+            // the name should only contain a first name, so ignore the second part if any
+            cleanedName = recognizedPlayerName.substring(recognizedPlayerName.indexOf(" "));
+        } else {
+            cleanedName = recognizedPlayerName;
+        }
+
+        // if the name is on our blacklist, it must be mis-recognition
+        if (NAME_BLACKLIST.contains(cleanedName)) {
+            return null;
+        }
+
+        return cleanedName;
+    }
+}
